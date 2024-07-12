950666623a9d5c0fc96fac410a04b83ebde1b3e2
Semitransparent Observer <138802968+SemiObserver@users.noreply.github.com>
2024-07-12 20:53:45 +0800

    FULL

---
+++ nano_bert/dataload_imdb.py
@@ -0,0 +1,163 @@
+import torch
+import random
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def encode_label(label):
+    if label == 'pos':
+        return 1
+    elif label == 'neg':
+        return 0
+    raise Exception(f'Unknown Label: {label}!')
+
+
+class IMDBDataloader:
+    def __init__(self, data, test_data, tokenizer, label_encoder, batch_size, val_frac=0.2):
+        train_data, val_data = train_test_split(data, shuffle=True, random_state=1, test_size=val_frac)
+
+        self.splits = {
+            'train': [d['text'] for d in train_data],
+            'test': [d['text'] for d in test_data],
+            'val': [d['text'] for d in val_data]
+        }
+
+        self.labels = {
+            'train': [d['label'] for d in train_data],
+            'test': [d['label'] for d in test_data],
+            'val': [d['label'] for d in val_data]
+        }
+
+        self.tokenized = {
+            'train': [tokenizer(record).unsqueeze(0) for record in
+                      tqdm(self.splits['train'], desc='Train Tokenization',position=0)], # divide different sentences in comments
+            'test': [tokenizer(record).unsqueeze(0) for record in tqdm(self.splits['test'], desc='Test Tokenization',position=0)],
+            'val': [tokenizer(record).unsqueeze(0) for record in tqdm(self.splits['val'], desc='Val Tokenization',position=0)],
+        }
+
+        self.encoded_labels = {
+            'train': [label_encoder(label) for label in tqdm(self.labels['train'], desc='Train Label Encoding',position=0)],
+            'test': [label_encoder(label) for label in tqdm(self.labels['test'], desc='Test Label Encoding',position=0)],
+            'val': [label_encoder(label) for label in tqdm(self.labels['val'], desc='Val Label Encoding',position=0)],
+        }
+
+        self.curr_batch = 0
+        self.batch_size = batch_size
+        self.iterate_split = None
+
+    def peek(self, split):
+        return {
+            'input_ids': self.splits[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)],
+            'label_ids': self.labels[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)],
+        }
+
+    def take(self, split):
+        batch = self.splits[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)]
+        labels = self.labels[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)]
+        self.curr_batch += 1
+        return {
+            'input_ids': batch,
+            'label_ids': labels,
+        }
+
+    def peek_tokenized(self, split):
+        return {
+            'input_ids': torch.cat(
+                self.tokenized[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)],
+                dim=0),
+            'label_ids': torch.tensor(
+                self.encoded_labels[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)],
+                dtype=torch.long),
+        }
+
+    def peek_index_tokenized(self, index, split):
+        return {
+            'input_ids': torch.cat(
+                [self.tokenized[split][index]],
+                dim=0),
+            'label_ids': torch.tensor(
+                [self.encoded_labels[split][index]],
+                dtype=torch.long),
+        }
+
+    def peek_index(self, index, split):
+        return {
+            'input_ids': [self.splits[split][index]],
+            'label_ids': [self.labels[split][index]],
+        }
+
+    def take_tokenized(self, split):
+        batch = self.tokenized[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)]
+        labels = self.encoded_labels[split][self.batch_size * self.curr_batch:self.batch_size * (self.curr_batch + 1)]
+        self.curr_batch += 1
+        return {
+            'input_ids': torch.cat(batch, dim=0),
+            'label_ids': torch.tensor(labels, dtype=torch.long),
+        }
+
+    def get_split(self, split):
+        self.iterate_split = split
+        return self
+
+    def steps(self, split):
+        return len(self.tokenized[split]) // self.batch_size
+
+    def __iter__(self):
+        self.reset()
+        return self
+
+    def __next__(self):
+        if self.batch_size * self.curr_batch < len(self.splits[self.iterate_split]):
+            return self.take_tokenized(self.iterate_split)
+        else:
+            raise StopIteration
+
+    def reset(self):
+        self.curr_batch = 0
+        
+def mask_text(cmt, vacab_size, max_seq_len, n_sp): # cmt : a comment. n_sp: number of special symbols in tokenizer
+    mps = []
+    mcmt = cmt.clone()
+    unique_elements, counts = torch.unique(cmt, return_counts=True)
+    m_range = (max_seq_len - counts[0].item()) * 0.15 # range of masking indice
+#     print(m_range, counts[0].item())
+    while len(mps) < m_range:
+        temp = random.randint(0, max_seq_len - 1)
+        if mcmt[temp] > n_sp - 1:
+            mps.append(temp)
+            mcmt[temp] = 0 # set to mask
+    temp_m = random.sample(range(0, len(mps) - 1), 2 * int(m_range * 0.1 + 1)) # fetch some masked words to their original value
+    half = int(len(temp_m)/2)
+    tmps = torch.tensor(mps) # tensorlize mps to get location of masks to be changed
+    mcmt[tmps[temp_m[:half]]] = cmt[temp_m[:half]] # 10% percent original
+    mcmt[tmps[temp_m[half:]]] = torch.tensor([random.randint(n_sp, vacab_size - 1) for i in range(half)]).to(device) # 10% percent random vocab
+    return mps, mcmt
+
+def spit_cmt(cmt): # split  comment into sentences
+    bg = torch.where(cmt==5)[0] # begin at '[SOS]'
+    ed = torch.where(cmt==6)[0] # end at '.'
+    spit = []
+    n_st = len(bg) # number of next sentence prediction task in this comment
+    for i in range(n_st):
+        sts = cmt[bg[i].item():ed[i].item()+1]
+        spit.append(sts)
+    return spit, n_st
+
+def pad_nsp(nsp, max_seq_len): # add '[CLS]' and padding on given composed 2 sentences in GPU
+#     print('len nsp = ', len(nsp))
+    return torch.cat((torch.tensor([0]).to(device), nsp, torch.ones(max_seq_len - len(nsp) - 1).to(device)), 0)
+
+def nsp_gen(spit, n_st, max_seq_len): # generate nsp input      
+    nsp = torch.zeros((2 * (n_st - 1), max_seq_len)).to(device)
+    if n_st < 3:
+        nsp = torch.zeros((1, max_seq_len)).to(device)
+#     print(nsp[:10], n_st)
+    for id_i in range(n_st - 1):
+        nsp[2 * id_i] = pad_nsp(torch.cat((spit[id_i], spit[id_i + 1]), 0), max_seq_len) # a sentence followed by the next
+        if n_st > 2:
+            id_s = random.randint(0, n_st - 1) # selected id for nsp
+            while id_s == id_i + 1 or id_s == id_i: # excluded the next sentence
+                id_s = random.randint(0, n_st - 1)
+            nsp[2 * id_i + 1] = pad_nsp(torch.cat((spit[id_i], spit[id_s]), 0), max_seq_len) # a sentence not followed by the next
+    return nsp.long().to(device)
--- nano_bert/model.py
+++ nano_bert/model.py
@@ -3,30 +3,64 @@ import math
 import torch
 import torch.nn.functional as F
 
+import torch
+import torch.nn as nn
 
-class BertEmbeddings(torch.nn.Module):
-    def __init__(self, vocab_size, n_embed, max_seq_len): # n_embed = 3, max_seq_len = 16
-        super().__init__()
-        self.max_seq_len = max_seq_len
-
-        self.word_embeddings = torch.nn.Embedding(vocab_size, n_embed) # number of words is length of text, each words has length n_embed
-        self.pos_embeddings = torch.nn.Embedding(max_seq_len, n_embed)
+class PositionalEncoding(nn.Module): # for BERT and ALBERT
+    def __init__(self, n_embed, max_seq_len):
+        super(PositionalEncoding, self).__init__()
+        
+        # Create a matrix of shape (max_len, d_model) with positional encodings
+        pe = torch.zeros(max_seq_len, n_embed)
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+        
+        # Div term represents the frequency of the sine and cosine functions
+        div_term = torch.exp(torch.arange(0, n_embed, 2).float() * (-torch.log(torch.tensor(10000.0)) / n_embed))
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        # Add a batch dimension for broadcasting
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        
+        # Register pe as a buffer, which means it's not a parameter but should be part of the state
+        self.register_buffer('pe', pe)
+        
 
         self.layer_norm = torch.nn.LayerNorm(n_embed, eps=1e-12, elementwise_affine=True) # eps: added as sqrt(var + eps) to prevent zero denominator
         self.dropout = torch.nn.Dropout(p=0.1, inplace=False) # inplace=False: do not replace the input by dropouted input
+    
+    def forward(self, x):
+        # Add positional encoding to the input embeddings
+        x = x + self.pe[:x.size(0), :]
+        embeddings = self.layer_norm(x)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+    
+class BertEmbeddings(torch.nn.Module):
+    def __init__(self, vocab_size, n_embed): # n_embed = 3, max_seq_len = 16
+        super().__init__()
+
+        self.word_embeddings = torch.nn.Embedding(vocab_size, n_embed) # number of words is length of text, each words has length n_embed
 
     def forward(self, x):
-        position_ids = torch.arange(self.max_seq_len, dtype=torch.long, device=x.device)
-
         words_embeddings = self.word_embeddings(x)
-        position_embeddings = self.pos_embeddings(position_ids)
 
-        embeddings = words_embeddings + position_embeddings
-        embeddings = self.layer_norm(embeddings)
-        embeddings = self.dropout(embeddings)
+        return words_embeddings
+    
+class ALBertEmbeddings(torch.nn.Module):
+    def __init__(self, vocab_size, n_embed, n_hid = 3): # n_embed = 3
+        super().__init__()
 
-        return embeddings
+        self.hid_embeddings = torch.nn.Embedding(vocab_size, n_hid) # number of words is length of text, each words has length n_embed
 
+        self.word_embeddings = torch.nn.Embedding(n_hid, n_embed) # number of words is length of text, each words has length n_embed
+    def forward(self, x):
+        hid_embeddings = self.hid_embeddings(x)
+        
+        words_embeddings = self.word_embeddings(hid_embeddings)
+
+        return words_embeddings
 
 class BertAttentionHead(torch.nn.Module):
     """
@@ -136,7 +170,7 @@ class BertLayer(torch.nn.Module):
 
 
 class BertEncoder(torch.nn.Module):
-    def __init__(self, n_layers, n_heads, dropout, n_embed): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3
+    def __init__(self, n_layers, n_heads, dropout, n_embed): # n_layers=2, n_heads=1, dropout=0.1, n_embed=4
         super().__init__()
 
         self.layers = torch.nn.ModuleList([BertLayer(n_heads, dropout, n_embed) for _ in range(n_layers)])
@@ -144,9 +178,18 @@ class BertEncoder(torch.nn.Module):
     def forward(self, x, mask):
         for layer in self.layers:
             x = layer(x, mask)
-
         return x
 
+class ALBertEncoder(torch.nn.Module):
+    def __init__(self, n_layers, n_heads, dropout, n_embed): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3
+        super().__init__()
+        self.n_layers = n_layers
+        self.layer = BertLayer(n_heads, dropout, n_embed)
+
+    def forward(self, x, mask):
+        for i in range(n_layers):
+            x = layer(x, mask)
+        return x
 
 class BertPooler(torch.nn.Module):
     def __init__(self, dropout, n_embed): # dropout=0.1, n_embed=3
@@ -163,129 +206,73 @@ class BertPooler(torch.nn.Module):
 
 
 class NanoBERT(torch.nn.Module):
-    """
-    NanoBERT is a almost an exact copy of a transformer decoder part described in the paper "Attention is all you need"
-    This is a base model that can be used for various purposes such as Masked Language Modelling, Classification,
-    Or any other kind of NLP tasks.
-    This implementation does not cover the Seq2Seq problem, but can be easily extended to that.
-    """
-
     def __init__(self, vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len): # n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len = 16
-        """
-
-        :param vocab_size: size of the vocabulary that tokenizer is using
-        :param n_layers: number of BERT layer in the model (default=2)
-        :param n_heads: number of heads in the MultiHeaded Self Attention Mechanism (default=1)
-        :param dropout: hidden dropout of the BERT model (default=0.1)
-        :param n_embed: hidden embeddings dimensionality (default=3)
-        :param max_seq_len: max length of the input sequence (default=16)
-        """
         super().__init__()
-
-        self.embedding = BertEmbeddings(vocab_size, n_embed, max_seq_len)
-
+        self.embedding = BertEmbeddings(vocab_size, n_embed)
+#         self.embedding = ALBertEmbeddings(vocab_size, n_embed)
+#         self.embedding = DEBertAEmbeddings(vocab_size, max_seq_len, n_embed-1, 1)
+        self.position = PositionalEncoding(n_embed, max_seq_len)
         self.encoder = BertEncoder(n_layers, n_heads, dropout, n_embed)
-
         self.pooler = BertPooler(dropout, n_embed)
 
     def forward(self, x):
         # attention masking for padded token
         # (batch_size, seq_len, seq_len)
-        mask = (x > 0).unsqueeze(1).repeat(1, x.size(1), 1)
-
-        embeddings = self.embedding(x)
-
-        encoded = self.encoder(embeddings, mask)
-
+        mask = (x != 1).unsqueeze(1).repeat(1, x.size(1), 1)
+        embeddings = self.embedding(x) 
+        position_embeddings = self.position(embeddings)
+        encoded = self.encoder(position_embeddings, mask)
         pooled = self.pooler(encoded)
         return pooled
 
-
-class NanoBertForClassification(torch.nn.Module):
-    """
-    This is a wrapper on the base NanoBERT that is used for classification task
-    One can use this as an example of how to extend and apply nano-BERT to similar custom tasks
-    This layer simply adds one additional dense layer for classification
-    """
-
-    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=16, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
-        super().__init__()
-        self.nano_bert = NanoBERT(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
-
-        self.classifier = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
-
-    def forward(self, input_ids):
-        embeddings = self.nano_bert(input_ids)
-
-        logits = self.classifier(embeddings)
-        return logits
-    
-class NextSentencePrediction(torch.nn.Module):
-    """
-    2-class classification model : is_next, is_not_next
-    """
-
-    def __init__(self, hidden):
-        """
-        :param hidden: BERT model output size
-        """
-        super().__init__()
-        self.linear = nn.Linear(hidden, 2)
-        self.softmax = nn.LogSoftmax(dim=-1)
-
-    def forward(self, x):
-        return self.softmax(self.linear(x[:, 0]))
-
-
-class MaskedLanguageModel(torch.nn.Module):
-    """
-    predicting origin token from masked input sequence
-    n-class classification problem, n-class = vocab_size
-    """
-
-    def __init__(self, hidden, vocab_size):
-        """
-        :param hidden: output size of BERT model
-        :param vocab_size: total vocab size
-        """
-        super().__init__()
-        self.linear = nn.Linear(hidden, vocab_size)
-        self.softmax = nn.LogSoftmax(dim=-1)
-
-    def forward(self, x):
-        return self.softmax(self.linear(x))
-
-class BertMix(torch.nn.Module):
-    """
-    This is a wrapper on the base NanoBERT that is used for classification task
-    One can use this as an example of how to extend and apply nano-BERT to similar custom tasks
-    This layer simply adds one additional dense layer for classification
-    """
-
-    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=16, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
-        super().__init__()
-        self.nano_bert = NanoBERT(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
-
-        self.classifier = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
-        self.mlm = torch.nn.Linear(in_features=n_embed, out_features=vocab_size)
-
-    def forward(self, input_ids):
-        embeddings = self.nano_bert(input_ids)
-
-        r_cls = self.classifier(embeddings)
-        r_mlm = self.mlm(embeddings)
-        return r_cls, r_mlm
     
 class BertMix3(torch.nn.Module):
+    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=128, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
+        super().__init__()
+        self.nano_bert = NanoBERT(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
+
+        self.cls= torch.nn.Linear(in_features=n_embed, out_features=n_classes)
+        self.mlm = torch.nn.Linear(in_features=n_embed, out_features=vocab_size)
+        self.nsp = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
+
+    def forward(self, input_ids):
+        embeddings = self.nano_bert(input_ids)
+
+        r_cls = self.cls(embeddings)
+        r_mlm = self.mlm(embeddings)
+        r_nsp = self.nsp(embeddings)
+        return r_cls, r_mlm, r_nsp
+
+class NanoALBERT(torch.nn.Module):
+    def __init__(self, vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len): # n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len = 16
+        super().__init__()
+#         self.embedding = BertEmbeddings(vocab_size, n_embed)
+        self.embedding = ALBertEmbeddings(vocab_size, n_embed)
+#         self.embedding = DEBertAEmbeddings(vocab_size, max_seq_len, n_embed-1, 1)
+        self.position = PositionalEncoding(n_embed, max_seq_len)
+        self.encoder = ALBertEncoder(n_layers, n_heads, dropout, n_embed)
+        self.pooler = BertPooler(dropout, n_embed)
+
+    def forward(self, x):
+        # attention masking for padded token
+        # (batch_size, seq_len, seq_len)
+        mask = (x != 1).unsqueeze(1).repeat(1, x.size(1), 1)
+        embeddings = self.embedding(x) 
+        position_embeddings = self.position(embeddings)
+        encoded = self.encoder(position_embeddings, mask)
+        pooled = self.pooler(encoded)
+        return pooled
+    
+class ALBertMix3(torch.nn.Module):
     """
     This is a wrapper on the base NanoBERT that is used for classification task
     One can use this as an example of how to extend and apply nano-BERT to similar custom tasks
     This layer simply adds one additional dense layer for classification
     """
 
-    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=16, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
+    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=128, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
         super().__init__()
-        self.nano_bert = NanoBERT(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
+        self.nano_bert = NanoALBERT(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
 
         self.classifier = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
         self.mlm = torch.nn.Linear(in_features=n_embed, out_features=vocab_size)
@@ -298,4 +285,174 @@ class BertMix3(torch.nn.Module):
         r_mlm = self.mlm(embeddings)
         r_nsp = self.nsp(embeddings)
         return r_cls, r_mlm, r_nsp
+####################################################################################################
+class AbsolutePositionEmbedding(torch.nn.Module): # for DEBERTA
+    def __init__(self, n_embed, max_seq_len):
+        super(AbsolutePositionEmbedding, self).__init__()
+        
+        self.max_seq_len = max_seq_len
+        
+        self.position_embeddings = nn.Embedding(max_seq_len, n_embed)
+        
+    def forward(self, input_ids):
+        # Get the position IDs from the input IDs
+        position_ids = torch.arange(0, self.max_seq_len, dtype=torch.long, device=input_ids.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
+        
+        # Get the position embeddings
+        position_embeddings = self.position_embeddings(position_ids)
+        
+        return position_embeddings
+    
+class DEBertAEmbeddings(torch.nn.Module):
+    def __init__(self, vocab_size, max_seq_len, n_embed_word = 3, n_embed_pos = 1): 
+        super().__init__()
 
+        self.word_embeddings = torch.nn.Embedding(vocab_size, n_embed_word) # number of words is length of text, each words has length n_embed
+
+        self.abposit_embeddings = AbsolutePositionEmbedding(n_embed_pos, max_seq_len)
+    def forward(self, x):
+        
+        words_embeddings = self.word_embeddings(x)
+        
+        abposits_embeddings = self.abposit_embeddings(x)
+
+        return torch.cat((words_embeddings, abposits_embeddings), dim = 2)
+
+class DEBertAAttentionHead(torch.nn.Module):
+
+    def __init__(self, head_size, dropout, n_embed): # dropout = 0.1, n_embed = 3
+        super().__init__()
+
+        self.query = torch.nn.Linear(in_features=n_embed, out_features=head_size)
+        self.key = torch.nn.Linear(in_features=n_embed, out_features=head_size)
+        self.values = torch.nn.Linear(in_features=n_embed, out_features=head_size)
+
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, I, x, mask):
+        B, seq_len, n_embed = x.shape
+
+        q = self.query(I)
+        k = self.key(x)
+        v = self.values(x)
+
+        weights = (q @ k.transpose(-2, -1)) / math.sqrt(n_embed)  # (B, Seq_len, Seq_len)
+        weights = weights.masked_fill(mask == 0, -1e9)  # mask out not attended tokens
+
+        scores = F.softmax(weights, dim=-1)
+        scores = self.dropout(scores)
+
+        context = scores @ v
+
+        return context
+
+
+class DEBertASelfAttention(torch.nn.Module):
+    """
+    MultiHeaded Self-Attention mechanism as described in "Attention is all you need"
+    """
+
+    def __init__(self, n_heads, dropout, n_embed): # , n_heads = 1, dropout = 0.1, n_embed = 3
+        super().__init__()
+
+        head_size = n_embed // n_heads
+
+        n_heads = n_heads
+
+        self.heads = torch.nn.ModuleList([DEBertAAttentionHead(head_size, dropout, n_embed) for _ in range(n_heads)])
+
+        self.proj = torch.nn.Linear(head_size * n_heads, n_embed)  # project from multiple heads to the single space
+
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, I, x, mask):
+        context = torch.cat([head(I, x, mask) for head in self.heads], dim=-1)
+
+        proj = self.proj(context)
+
+        out = self.dropout(proj)
+
+        return out
+class DEBertALayer(torch.nn.Module):
+    """
+    First layer of DEBERTA transformer model
+    """
+
+    def __init__(self, n_heads, dropout, n_embed): # n_heads=1, dropout=0.1, n_embed=3
+        super().__init__()
+
+        # unlike in the original paper, today in transformers it is more common to apply layer norm before other layers
+        # this idea is borrowed from Andrej Karpathy's series on transformers implementation
+        self.layer_norm1 = torch.nn.LayerNorm(n_embed)
+        self.self_attention = DEBertASelfAttention(n_heads, dropout, n_embed)
+
+        self.layer_norm2 = torch.nn.LayerNorm(n_embed)
+        self.feed_forward = FeedForward(dropout, n_embed)
+
+    def forward(self, I, x, mask):
+        x = self.layer_norm1(x)
+        x = x + self.self_attention(I, x, mask)
+
+        x = self.layer_norm2(x)
+        out = x + self.feed_forward(x)
+
+        return out
+    
+class DEBertAEncoder(torch.nn.Module):
+    def __init__(self, n_layers, n_heads, dropout, n_embed): # n_layers=2, n_heads=1, dropout=0.1, n_embed=4
+        super().__init__()
+
+        self.layers = torch.nn.ModuleList([DEBertALayer(n_heads, dropout, n_embed) for _ in range(n_layers)])
+
+    def forward(self, I, x, mask):
+        i = 0
+        for layer in self.layers:
+            if i == 0:
+                x = layer(I, x, mask)
+            else:
+                x = layer(x, x, mask)
+        return x
+    
+class NanoDEBERTA(torch.nn.Module):
+    def __init__(self, vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len): # n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len = 16
+        super().__init__()
+#         self.embedding = BertEmbeddings(vocab_size, n_embed)
+#         self.embedding = ALBertEmbeddings(vocab_size, n_embed)
+        self.embedding = DEBertAEmbeddings(vocab_size, max_seq_len, n_embed-1, 1)
+        self.embedding_I = AbsolutePositionEmbedding(n_embed, max_seq_len)
+        self.encoder = DEBertAEncoder(n_layers, n_heads, dropout, n_embed)
+        self.pooler = BertPooler(dropout, n_embed)
+
+    def forward(self, x):
+        # attention masking for padded token
+        # (batch_size, seq_len, seq_len)
+        mask = (x != 1).unsqueeze(1).repeat(1, x.size(1), 1)
+        embeddings = self.embedding(x) 
+        embeddings_I = self.embedding_I(x)
+        encoded = self.encoder(embeddings_I, embeddings, mask)
+        pooled = self.pooler(encoded)
+        return pooled
+    
+class DEBertAMix3(torch.nn.Module):
+    """
+    This is a wrapper on the base NanoBERT that is used for classification task
+    One can use this as an example of how to extend and apply nano-BERT to similar custom tasks
+    This layer simply adds one additional dense layer for classification
+    """
+
+    def __init__(self, vocab_size, n_layers=2, n_heads=1, dropout=0.1, n_embed=4, max_seq_len=128, n_classes=2): # n_layers=2, n_heads=1, dropout=0.1, n_embed=3, max_seq_len=16, n_classes=2
+        super().__init__()
+        self.nano_bert = NanoDEBERTA(vocab_size, n_layers, n_heads, dropout, n_embed, max_seq_len)
+
+        self.classifier = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
+        self.mlm = torch.nn.Linear(in_features=n_embed, out_features=vocab_size)
+        self.nsp = torch.nn.Linear(in_features=n_embed, out_features=n_classes)
+
+    def forward(self, input_ids):
+        embeddings = self.nano_bert(input_ids)
+
+        r_cls = self.classifier(embeddings)
+        r_mlm = self.mlm(embeddings)
+        r_nsp = self.nsp(embeddings)
+        return r_cls, r_mlm, r_nsp
